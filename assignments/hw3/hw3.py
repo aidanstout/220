402145558db91abd279311a9ec37868533d42235
aidanstout <stouta@g.cofc.edu>
2022-02-04 23:11:00 -0500

    completed hw 3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -2,7 +2,8 @@
 Name: <your name goes here – first and last>
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Brief, one or two sentence description of the problem that this program solves,
+ in your own words.>
 
 Certification of Authenticity:
 <include one of the following>
@@ -12,23 +13,53 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def average():
-    pass
+    number_of_inputs = eval(input("How many grades will you enter?"))
+    grade_average = 0
+    for i in range(number_of_inputs):
+        grades = eval(input("Enter grade"))
+        grade_average = grade_average + grades
+    grade_average = grade_average / number_of_inputs
+    print("average is", grade_average)
 
 
 def tip_jar():
-    pass
+    total_tips = 0
+    for i in range(5):
+        tip_amount = eval(input("How much would you like to donate?"))
+        total_tips = total_tips + tip_amount
+    print('total tips:', total_tips)
 
 
 def newton():
-    pass
+    number_to_sqrt = eval(input("What number do you want to square root?"))
+    approx = 1
+    amount_of_approx = eval(input("How many times should we improve the approximation?"))
+    for i in range(amount_of_approx):
+        approx = ((approx + (number_to_sqrt / approx)) / 2)
+    print("The square root is approximately", approx)
 
 
 def sequence():
-    pass
+    terms = eval(input('how many terms would you like?'))
+    numbers = 1
+    print("1", end='\t')
+    for i in range(terms - 1):
+        numbers = numbers + (i % 2) + (i % 2)
+        print(numbers, end='\t')
 
 
 def pi():
-    pass
+    terms = eval(input('how many terms in the series?'))
+    numerator = 0
+    denominator = 1
+    results = 1
+    for i in range(terms):
+        numerator = numerator + (i - 1) % 2 + (i - 1) % 2
+        denominator = denominator + (i % 2) + (i % 2)
+        results = results * (numerator / denominator)
+    results = results * 2
+    print(results)
+
 
 
 if __name__ == '__main__':
