0062a6fb7e5112615e18e4dbc01de8c1a2fff644
aidanstout <stouta@g.cofc.edu>
2022-03-31 22:19:39 -0400

    completed hw 9

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -1,25 +1,58 @@
+"""
+Aidan Stout
+hw9.py
+Problem: create a hangman program
+I certify that this is entirely my work
+"""
+
+from random import randint
+
+
 def get_words(file_name):
-    pass
+    word_file = open(file_name, 'r')
+    words = []
+    lines = word_file.readlines()
+    for i in lines:
+        words.append(i)
+    return words
 
 
 def get_random_word(words):
-    pass
+    word_num = randint(1, len(words))
+    word = words[word_num]
+    word.strip()
+    return word
 
 
 def letter_in_secret_word(letter, secret_word):
-    pass
+    letter_index = secret_word.find(letter)
+    if letter_index >= 0:
+        return True
+    return False
 
 
 def already_guessed(letter, guesses):
-    pass
+    if letter in guesses:
+        return True
+    return False
 
 
 def make_hidden_secret(secret_word, guesses):
-    pass
+    word_len = len(secret_word)
+    hidden = ""
+    for i in range(word_len):
+        hidden = hidden + ' _'
+    hidden.strip()
+    for letter in secret_word:
+        if letter in guesses:
+            hidden.replace('_', letter)
+    return hidden
 
 
 def won(guessed):
-    pass
+    if '_' not in guessed:
+        return True
+    return False
 
 
 def play_graphics(secret_word):
@@ -27,10 +60,28 @@ def play_graphics(secret_word):
 
 
 def play_command_line(secret_word):
-    pass
+    guessed = []
+    guesses_remaining = 6
+    hidden = make_hidden_secret(secret_word, guessed)
+    while not won(hidden) and guesses_remaining > 0:
+        print("already guessed: {}".format(guessed))
+        print("guesses remaining: {}".format(guesses_remaining))
+        print(hidden)
+        letter = input("guess a letter: ")
+        while already_guessed(letter, guessed):
+            letter = input('guess again: ')
+        guessed.append(letter)
+        if letter_in_secret_word(letter, secret_word):
+            hidden = make_hidden_secret(secret_word, guessed)
+        else:
+            guesses_remaining = guesses_remaining - 1
+    if won(hidden):
+        print("winner! \n {}".format(hidden))
+    elif guesses_remaining < 1:
+        print('sorry, you did not guess the secret word. \n the secret word was {}'.format(secret_word))
 
 
-if __name__ == '__main__':
-    pass
+# if __name__ == '__main__':
+#     pass
     # play_command_line(secret_word)
     # play_graphics(secret_word)
